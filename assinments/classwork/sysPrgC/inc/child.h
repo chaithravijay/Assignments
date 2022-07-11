732c093af54e541fb13b84f9ebfd203e2bd7ba1e
chaithravijay <107975144+chaithravijay@users.noreply.github.com>
2022-07-11 19:57:19 +0530

    Update child.h

--- assinments/classwork/sysPrgC/inc/child.h
+++ assinments/classwork/sysPrgC/inc/child.h
@@ -5,26 +5,3 @@
 #include <sys/wait.h>
 
 using namespace std;
-int main(int argc, char*argv)
-{
- int N;
- N = fork();
- if(N%2! = 0)
- {
-    for(int i=0;i<100;i++)
-     cout<<"odd numbers:"<<endl;
-     cout<<"i="<<i<<endl;\
-     exit(0);
-}
-else
-{
-  if(N%2 ==0){
-   sleep(10);
-   for(int j=0;j=100;j++)
-   cout<<"even numbers:"<<endl;
-   cout<<"j="<<j<<endl;
-}
-}
-waitpid(0);
-return 0;
-}
