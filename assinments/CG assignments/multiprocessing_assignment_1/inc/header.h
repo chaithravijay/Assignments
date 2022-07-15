91073ce447733db72bbc3079baee9d2e604905e3
chaithravijay <107975144+chaithravijay@users.noreply.github.com>
2022-07-15 09:31:06 +0530

    Add files via upload

---
+++ assinments/CG assignments/multiprocessing_assignment_1/inc/header.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+using namespace std;
